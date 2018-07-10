01001a89b9c9d85e298fd9f0576c33020314011c
Fabian Loeschner <fabian.loeschner@rwth-aachen.de>
2018-07-10 11:25:43 +0200

    Fixes after rebase

--- minidolfin/assembling.py
+++ minidolfin/assembling.py
@@ -1,4 +1,5 @@
 import tsfc
+import tsfc.fiatinterface
 import dijitso
 import ffc.compiler
 from coffee.plan import ASTKernel
@@ -11,7 +12,7 @@ import ctypes
 import hashlib
 
 
-def tsfc_compile_wrapper(form, extra_parameters = None):
+def tsfc_compile_wrapper(form, extra_parameters=None, **kwargs):
     """Compiles form with TSFC and returns source code."""
 
     parameters = {'mode': 'spectral'}
@@ -30,7 +31,7 @@ def tsfc_compile_wrapper(form, extra_parameters = None):
     return code
 
 
-def ffc_compile_wrapper(form, extra_parameters = None):
+def ffc_compile_wrapper(form, extra_parameters=None, cross_element_width=0, gcc_vec_ext=False, **kwargs):
     """Compiles form with FFC and returns source code."""
 
     parameters = ffc.parameters.default_parameters()
@@ -74,7 +75,8 @@ def ffc_compile_wrapper(form, extra_parameters = None):
 
     return code
 
-def compile_form(a, form_compiler=None, form_compiler_parameters=None):
+
+def compile_form(a, form_compiler=None, form_compiler_parameters=None, **kwargs):
     """Compiles form with the specified compiler and returns a ctypes function ptr."""
 
     # Use tsfc as default form compiler
@@ -102,7 +104,8 @@ def compile_form(a, form_compiler=None, form_compiler_parameters=None):
              'cxx': 'cc',
             'cxxflags': (
                 '-O2', '-Wall', '-shared', '-fPIC', '-std=c11',
-                '-march=skylake', '-mtune=skylake', '-ftree-vectorize', '-funroll-loops'),
+                '-march=skylake', '-mtune=skylake', '-ftree-vectorize', '-funroll-loops'
+            ),
          },
          'cache': {'src_postfix': '.c'},
     }
@@ -126,9 +129,59 @@ ADD_VALUES = PETSc.InsertMode.ADD_VALUES
 del petsc
 
 
+def assemble(petsc_tensor, dofmap, form, **kwargs):
+    assert len(form.arguments()) == 2, "Now only bilinear forms"
+
+    # JIT compile UFL form into ctypes function
+    assembly_kernel = compile_form(form, **kwargs)
+
+    # Fetch data
+    tdim = dofmap.mesh.reference_cell.get_dimension()
+    cells = dofmap.mesh.get_connectivity(tdim, 0)
+    vertices = dofmap.mesh.vertices
+    cell_dofs = dofmap.cell_dofs
+    mat = petsc_tensor.handle
+
+    # Prepare cell tensor temporary
+    elements = tuple(arg.ufl_element() for arg in form.arguments())
+    fiat_elements = map(tsfc.fiatinterface.create_element, elements)
+    element_dims = tuple(fe.space_dimension() for fe in fiat_elements)
+    _A = numpy.zeros(element_dims, dtype=numpy.double)
+
+    # Prepare coordinates temporary
+    num_vertices_per_cell = cells.shape[1]
+    gdim = vertices.shape[1]
+    _coords = numpy.zeros((num_vertices_per_cell, gdim), dtype=numpy.double)
+
+    @numba.jit(nopython=True)
+    def _assemble(assembly_kernel, cells, vertices, cell_dofs, mat, _coords, _A):
+        coords_ptr = _coords.ctypes.data
+        A_ptr = _A.ctypes.data
+        nrows = ncols = cell_dofs.shape[1]
+
+        # Loop over cells
+        for i in range(cells.shape[0]):
+            # Update temporaries
+            _coords[:] = vertices[cells[i]]
+            _A[:] = 0
+
+            # Assemble cell tensor
+            assembly_kernel(A_ptr, coords_ptr)
+
+            # Add to global tensor
+            rows = cols = cell_dofs[i].ctypes.data
+            ierr = MatSetValues(mat, nrows, rows, ncols, cols, A_ptr, ADD_VALUES)
+            assert ierr == 0
+
+    # Call jitted hot loop
+    _assemble(assembly_kernel, cells, vertices, cell_dofs, mat, _coords, _A)
+
+    petsc_tensor.assemble()
+
+
 def empty_aligned(shape, dtype, align):
     """
-    Allocate numpy array with a certain memory alignment.
+    Allocate numpy array with a specified memory alignment.
 
     :param shape: Shape of the requested array.
     :param dtype: Data-type of the requested array.
@@ -159,7 +212,9 @@ def empty_aligned(shape, dtype, align):
 def assemble_vectorized(petsc_tensor, dofmap, form, **kwargs):
     assert len(form.arguments()) == 2, "Now only bilinear forms"
 
+    # Size of cross-element batch
     vec_width = 4
+
     # JIT compile UFL form into ctypes function
     assembly_kernel = compile_form(form,
                                    form_compiler="ffc",
@@ -228,56 +283,5 @@ def assemble_vectorized(petsc_tensor, dofmap, form, **kwargs):
 
     # Call jitted hot loop
     _assemble(assembly_kernel, cells, vertices, cell_dofs, mat, _coords, _A, _coords_t, _A_t)
-
-    petsc_tensor.assemble()
-
-
-def assemble(petsc_tensor, dofmap, form, **kwargs):
-    assert len(form.arguments()) == 2, "Now only bilinear forms"
-
-    # JIT compile UFL form into ctypes function
-    assembly_kernel = compile_form(form, **kwargs)
-
-    # Fetch data
-    tdim = dofmap.mesh.reference_cell.get_dimension()
-    cells = dofmap.mesh.get_connectivity(tdim, 0)
-    vertices = dofmap.mesh.vertices
-    cell_dofs = dofmap.cell_dofs
-    mat = petsc_tensor.handle
-
-    # Prepare cell tensor temporary
-    elements = tuple(arg.ufl_element() for arg in form.arguments())
-    fiat_elements = map(tsfc.fiatinterface.create_element, elements)
-    element_dims = tuple(fe.space_dimension() for fe in fiat_elements)
-    _A = numpy.zeros(element_dims, dtype=numpy.double)
-
-    # Prepare coordinates temporary
-    num_vertices_per_cell = cells.shape[1]
-    gdim = vertices.shape[1]
-    _coords = numpy.zeros((num_vertices_per_cell, gdim), dtype=numpy.double)
-
-    @numba.jit(nopython=True)
-    def _assemble(assembly_kernel, cells, vertices, cell_dofs, mat, _coords, _A):
-        coords_ptr = _coords.ctypes.data
-        A_ptr = _A.ctypes.data
-        nrows = ncols = cell_dofs.shape[1]
-
-        # Loop over cells
-        for i in range(cells.shape[0]):
-
-            # Update temporaries
-            _coords[:] = vertices[cells[i]]
-            _A[:] = 0
-
-            # Assemble cell tensor
-            assembly_kernel(A_ptr, coords_ptr)
-
-            # Add to global tensor
-            rows = cols = cell_dofs[i].ctypes.data
-            ierr = MatSetValues(mat, nrows, rows, ncols, cols, A_ptr, ADD_VALUES)
-            assert ierr == 0
-
-    # Call jitted hot loop
-    _assemble(assembly_kernel, cells, vertices, cell_dofs, mat, _coords, _A)
 
     petsc_tensor.assemble()
