9525ce6923c37d5802ff9b1c1db4fc6c5134bec6
Fabian Loeschner <fabian.loeschner@rwth-aachen.de>
2018-07-13 21:32:58 +0200

    Use UFC interface for TSFC, add coefficient pointer to signature

--- minidolfin/assembling.py
+++ minidolfin/assembling.py
@@ -1,8 +1,12 @@
-import tsfc
 import dijitso
-import ffc.compiler
-from coffee.plan import ASTKernel
 from ufl.utils.sorting import canonicalize_metadata
+
+import ffc.compiler
+
+import tsfc
+import tsfc.fiatinterface, tsfc.kernel_interface, tsfc.ufl_utils, tsfc.driver
+import tsfc.kernel_interface.ufc as ufc_interface
+from coffee.plan import ASTKernel
 
 import numpy
 import numba
@@ -21,15 +25,20 @@ def tsfc_compile_wrapper(form, parameters=None):
     parameters_ = {'mode': 'spectral'}
     parameters_.update(parameters or {})
 
-    kernel, = tsfc.compile_form(form, parameters=parameters_)
+    fd = tsfc.ufl_utils.compute_form_data(form)
 
-    k = ASTKernel(kernel.ast)
-    k.plan_cpu(dict(optlevel='Ov'))
+    codes = []
+    for id in fd.integral_data:
+        ast = tsfc.driver.compile_integral(id, fd, "form", parameters_, interface=tsfc.kernel_interface.ufc)
 
-    code = kernel.ast.gencode()
+        knl = ASTKernel(ast)
+        knl.plan_cpu(dict(optlevel='Ov'))
 
+        code = ast.gencode()
+        codes.append(code)
+
+    code = "\n\n".join(["#include <math.h>"] + codes)
     code = code.replace('static inline', '')
-    code = "#include <math.h>\n\n" + code
 
     return code
 
@@ -47,26 +56,25 @@ def ffc_compile_wrapper(form, parameters=None):
     form_index = 0
 
     # Extract tabulate_tensor definition
-    function_name = "tabulate_tensor_{}_cell_integral_{}_otherwise".format(prefix, form_index)
-    index_start = code_c.index("void {}(".format(function_name))
+    ffc_function_name = "tabulate_tensor_{}_cell_integral_{}_otherwise".format(prefix, form_index)
+    index_start = code_c.index("void {}(".format(ffc_function_name))
     index_end = code_c.index("ufc_cell_integral* create_{}_cell_integral_{}_otherwise(void)".format(prefix, form_index),
                              index_start)
     tabulate_tensor_code = code_c[index_start:index_end].strip()
 
-    # Extract tabulate_tensor body
-    body_start = tabulate_tensor_code.index("{")
-    tabulate_tensor_body = tabulate_tensor_code[body_start:].strip()
+    function_name = "form_cell_integral_otherwise"
+    tabulate_tensor_code = tabulate_tensor_code.replace(ffc_function_name, function_name)
 
-    tabulate_tensor_signature = "void form_cell_integral_otherwise (double* restrict A, const double *restrict coordinate_dofs)"
-
-    return "\n".join([
+    code = "\n".join([
         "#include <math.h>",
         "#include <stdalign.h>",
         "#include <string.h>",
+        "typedef double ufc_scalar_t;",
         "",
-        tabulate_tensor_signature,
-        tabulate_tensor_body,
+        tabulate_tensor_code
     ])
+
+    return code
 
 
 def jit_compile_form(a, parameters=None):
@@ -77,9 +85,10 @@ def jit_compile_form(a, parameters=None):
 
     # Use tsfc as default form compiler
     compiler = parameters.pop("compiler", "tsfc")
-    compile_form = {"ffc": ffc_compile_wrapper,
-                    "tsfc": tsfc_compile_wrapper
-                   }[compiler]
+    compile_form = {
+        "ffc": ffc_compile_wrapper,
+        "tsfc": tsfc_compile_wrapper
+    }[compiler]
 
     # Define generation function executed on cache miss
     def generate(form, name, signature, jit_params):
@@ -95,11 +104,11 @@ def jit_compile_form(a, parameters=None):
 
     # Set dijitso into C mode
     jit_params = {
-         'build': {
-             'cxx': 'cc',
-             'cxxflags': ('-Wall', '-shared', '-fPIC', '-std=c11'),
-         },
-         'cache': {'src_postfix': '.c'},
+        'build': {
+            'cxx': 'cc',
+            'cxxflags': ('-Wall', '-shared', '-fPIC', '-std=c11'),
+        },
+        'cache': {'src_postfix': '.c'},
     }
 
     # Do JIT compilation
@@ -107,7 +116,7 @@ def jit_compile_form(a, parameters=None):
 
     # Grab assembly kernel from ctypes module and set its arguments
     func = getattr(module, 'form_cell_integral_otherwise')
-    func.argtypes = (ctypes.c_void_p, ctypes.c_void_p)
+    func.argtypes = (ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p)
 
     return func
 
@@ -124,13 +133,16 @@ if libpetsc_path is None:
     libpetsc_path = os.path.join(petsc_dir, 'lib', 'libpetsc' + so)
 petsc = ctypes.CDLL(libpetsc_path)
 MatSetValues = petsc.MatSetValues
-MatSetValues.argtypes = 7*(ctypes.c_void_p,)
+MatSetValues.argtypes = 7 * (ctypes.c_void_p,)
 ADD_VALUES = PETSc.InsertMode.ADD_VALUES
 del petsc
 
 
 def assemble(petsc_tensor, dofmap, form, form_compiler_parameters=None):
     assert len(form.arguments()) == 2, "Now only bilinear forms"
+    assert len(form.coefficients()) == 0, "Coefficients not supported"
+    assert len({integral for integral in form.integrals() if integral.integral_type() != "cell"}) == 0, "Only cell " \
+                                                                                                        "integrals"
 
     # JIT compile UFL form into ctypes function
     assembly_kernel = jit_compile_form(form, form_compiler_parameters)
@@ -154,20 +166,19 @@ def assemble(petsc_tensor, dofmap, form, form_compiler_parameters=None):
     _coords = numpy.ndarray((num_vertices_per_cell, gdim), dtype=numpy.double)
 
     @numba.jit(nopython=True)
-    def _assemble(assembly_kernel, cells, vertices, cell_dofs, mat, _coords, _A):
+    def _assemble(assembly_kernel, cells, vertices, cell_dofs, mat, _coords, _A, _ws_ptr):
         coords_ptr = _coords.ctypes.data
         A_ptr = _A.ctypes.data
         nrows = ncols = cell_dofs.shape[1]
 
         # Loop over cells
         for i in range(cells.shape[0]):
-
             # Update temporaries
             _coords[:] = vertices[cells[i]]
             _A[:] = 0
 
             # Assemble cell tensor
-            assembly_kernel(A_ptr, coords_ptr)
+            assembly_kernel(A_ptr, _ws_ptr, coords_ptr)
 
             # Add to global tensor
             rows = cols = cell_dofs[i].ctypes.data
@@ -175,6 +186,6 @@ def assemble(petsc_tensor, dofmap, form, form_compiler_parameters=None):
             assert ierr == 0
 
     # Call jitted hot loop
-    _assemble(assembly_kernel, cells, vertices, cell_dofs, mat, _coords, _A)
+    _assemble(assembly_kernel, cells, vertices, cell_dofs, mat, _coords, _A, 0)
 
     petsc_tensor.assemble()
